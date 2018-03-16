7bfc0680d5e9becff9b5b8f3d06ab20066611546
qitian <qitian@boloomo.com>
2018-03-16 15:30:51 +0800

    no message

--- TestScrapy/ClimatePipelines.py
+++ TestScrapy/ClimatePipelines.py
@@ -1,51 +1,23 @@
+# coding=utf-8
 from scrapy.exceptions import DropItem
-from twisted.enterprise import adbapi
-import MySQLdb.cursors
-import codecs
-import json
-from logging import log
-import json
+import MySQLdb
 
 
 class ClimatePipeline(object):
-    @classmethod
-    def from_settings(cls, settings):
-        db_params = dict(
-            host=settings['MYSQL_HOST'],
-            db=settings['MYSQL_DBNAME'],
-            user=settings['MYSQL_USER'],
-            passwd=settings['MYSQL_PASSWD'],
-            charset='utf8',
-            cursorclass=MySQLdb.cursors.DictCursor,
-            use_unicode=True
-        )
-        dbpool = adbapi.ConnectionPool('MySQLdb', **db_params)
-        return cls(dbpool)
-
-    def __init__(self, dbpool):
-        self.dbpool = dbpool
+    def __init__(self):
+        # 初始化数据库对象
+        self.connect = MySQLdb.connect('192.168.129.187', 'root', '123456', 'climate', charset='utf8')
+        self.cursor = self.connect.cursor()
 
     def process_item(self, item, spider):
-        query = self.dbpool.runInteraction(self._conditional_update, item)
-        query.addErrback(self._handle_error, item, spider)
-        # return item
-
-    @staticmethod
-    def _conditional_update(tx, item):
         insert_climate_code = 'insert ignore into `climate_code`(`code`, `climate`) values(%s, %s)'
         replace_station_forecast = 'replace into `station_forecast`(`station_id`, `time`, `temp`, `prep`, `climate`, `wind_speed`, `wind_dire`, `air_pres`, `relative_hum`, `cloud`, `visibility`) values((select station_id from `station_location` where `station_name` = %s), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
         params_climate_code = (item['climate_code'], item['day_climate'])
-        params_station_forecast = (item['name'], item['time'], item['temp'],  item['prep'], item['climate'], item['wind_speed'], item['wind_dire'], item['air_pres'], item['relative_hum'], item['cloud'], item['visibility'])
-        tx.execute(insert_climate_code, params_climate_code)
-        tx.execute(replace_station_forecast, params_station_forecast)
-        # insert_station_forecast = 'insert into `station_forecast`(`station_id`, `time`, `temp`, `prep`, `climate`, `wind_speed`, `wind_dire`, `air_pres`, `relative_hum`, `cloud`, `visibility`)values((select `station_id` from `station_location` where `station_name` = %s), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s) on duplicate key update `temp` = %s, `prep` = %s, `climate` = %s, `wind_speed` = %s, `wind_dire` = %s, `air_pres` = %s, `relative_hum` = %s, `cloud` = %s, `visibility` = %s'
-        # params_station_forecast11 = (item['name'], item['time'], item['temp'],  item['prep'], item['climate'], item['wind_speed'], item['wind_dire'], item['air_pres'], item['relative_hum'], item['cloud'], item['visibility'], item['temp'],  item['prep'], item['climate'], item['wind_speed'], item['wind_dire'], item['air_pres'], item['relative_hum'], item['cloud'], item['visibility'])
-        # tx.execute(insert_station_forecast, params_station_forecast11)
+        params_station_forecast = (item['name'], item['time'], item['temp'], item['prep'], item['climate'], item['wind_speed'], item['wind_dire'], item['air_pres'], item['relative_hum'], item['cloud'], item['visibility'])
+        self.cursor.execute(insert_climate_code, params_climate_code)
+        self.cursor.execute(replace_station_forecast, params_station_forecast)
+        self.connect.commit()
 
-    @staticmethod
-    def _handle_error(failure, item, spider):
-        print("+++++++++++++++++++++++++++1111111+++++++++++++++++++++++++++")
-        print '--------------database operation exception!!-----------------'
-        print failure
-        print '--------------database operation exception!!-----------------'
-        print '+++++++++++++++++++++++++++1111111+++++++++++++++++++++++++++'
+    def closed_spider(self, spider):
+        self.cursor.close()
+        self.connect.close()
