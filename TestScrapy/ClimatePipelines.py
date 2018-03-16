2c516f4f23e941d8feb03c0e6002d01d9473e143
qitian <qitian@boloomo.com>
2018-03-16 11:26:13 +0800

    no message

--- TestScrapy/ClimatePipelines.py
+++ TestScrapy/ClimatePipelines.py
@@ -33,7 +33,7 @@ class ClimatePipeline(object):
     @staticmethod
     def _conditional_update(tx, item):
         insert_climate_code = 'insert ignore into `climate_code`(`code`, `climate`) values(%s, %s)'
-        insert_station_forecast = 'replace into `station_climate`(`station_id`, `time`, `temp`, `prep`, `climate`, `wind_speed`, `wind_dire`, `air_pres`, `relative_hum`, `cloud`, `visibility`) values((select station_id from `station_location` where `station_name` = %s), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
+        insert_station_forecast = 'replace into `station_forecast`(`station_id`, `time`, `temp`, `prep`, `climate`, `wind_speed`, `wind_dire`, `air_pres`, `relative_hum`, `cloud`, `visibility`) values((select station_id from `station_location` where `station_name` = %s), %s, %s, %s, %s, %s, %s, %s, %s, %s, %s)'
         params_climate_code = (item['climate_code'], item['day_climate'])
         params_station_forecast = (item['name'], item['time'], item['temp'],  item['prep'], item['climate'], item['wind_speed'], item['wind_dire'], item['air_pres'], item['relative_hum'], item['cloud'], item['visibility'])
         tx.execute(insert_climate_code, params_climate_code)
